6fca92d9d70f508e93969b20c7dad25f6a5d6d6a
Sanjay Developer <sureshsanjay805@gmail.com>
2021-01-17 18:42:00 +0530

    Task 4.1: Expandable CardView implemented

--- app/src/main/java/com/sanjaydevtech/asjxcalorietracker/DailyFragment.kt
+++ app/src/main/java/com/sanjaydevtech/asjxcalorietracker/DailyFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
+import androidx.vectordrawable.graphics.drawable.AnimationUtilsCompat
 import com.sanjaydevtech.asjxcalorietracker.databinding.FragmentDailyBinding
 
 class DailyFragment : Fragment() {
@@ -17,5 +20,19 @@ class DailyFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.button.setOnClickListener {
+            it.animate().apply {
+                duration = 300L
+                rotationBy(180f)
+            }
+            binding.imageView.visibility = if (binding.imageView.visibility == View.GONE) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 }
